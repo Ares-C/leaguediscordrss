125eb3638b8658c3aaee72850ae0f4be7a6df899
Ares (TC) <aresmta@gmail.com>
2019-03-22 20:14:55 -0300

    v2

--- leaguerss.py
+++ leaguerss.py
@@ -1,71 +1,105 @@
+######################################################
+ # Copyright (C) 2018 Tomás Caram <aresmta@gmail.com>
+ # 
+ # Unauthorized copying of this file, via any medium is strictly prohibited
+ # Proprietary and confidential
+ # Written by Tomás Caram <aresmta@gmail.com>, August 2018
+#######################################################
+
 from bs4 import BeautifulSoup
 import feedparser
 import requests
 import time
 
-URL_ROOT = 'https://las.leagueoflegends.com/es'
-WEBHOOK_URL = "https://discordapp.com/api/webhooks/0000000000000/00000000000000000000000000000"
+DATA = {
+    "WEBHOOK_URL": ["https://discordapp.com/api/webhooks/"],
+    "SERVERS": {
+        "LAS": {
+            "name": "Latinoamérica Sur",
+            "url": "https://las.leagueoflegends.com/",
+            "lang": "es"
+        },
+        "LAN": {
+            "name": "Latinoamérica Norte",
+            "url": "https://lan.leagueoflegends.com/",
+            "lang": "es"
+        }
+    }
+}
 
-# etag = ""
-# modified = ""
 guids = {}
+titles = {}
 
 def fetch_data():
-	global URL_ROOT, guids, etag, modified
-	feed = feedparser.parse(URL_ROOT+'/rss.xml')
-	# etag = feed.etag
-	# modified = feed.modified
+    global guids, titles
+    for server in DATA['SERVERS']:
+        url = DATA['SERVERS'][server]['url'] 
+        lang = DATA['SERVERS'][server]['lang'] 
 
-	for entry in feed.entries:
-		guid = entry.guid.split(" ")[0]
-		guids[guid] = True
+        feed = feedparser.parse(url+lang+"/rss.xml")
+        for entry in feed.entries:
+            guid = entry.guid.split(" ")[0]
+            title = entry.title
+
+            guids[guid] = True
+            titles[title] = True
 
 def check_for_new_articles():
-	# global URL_ROOT, guids, etag, modified
-	global URL_ROOT, guids
-	# feed = feedparser.parse(URL_ROOT+'/rss.xml', etag=etag, modified=modified)
-	feed = feedparser.parse(URL_ROOT+'/rss.xml')
-	if feed.status == 200:
-		for entry in feed.entries:
-			try:
-				entry_guid = entry.guid.split(" ")[0]
-			except:
-				continue
-			if entry_guid not in guids:
-				soup = BeautifulSoup(entry.description, "html.parser")
-				description = soup.find('div').getText()
-				image_url = URL_ROOT+soup.find('img')['src']
-				
-				post_to_discord(entry.title, description, entry.link, image_url, entry.published_parsed)
+    global guids, titles
+    for server in DATA['SERVERS']:
+        server_name = DATA['SERVERS'][server]['name']
+        url = DATA['SERVERS'][server]['url']
+        nurl = url[:-1]
+        lang = DATA['SERVERS'][server]['lang'] 
 
-				guids[entry_guid] = True
-	# etag = feed.etag
-	# modified = feed.modified
+        feed = feedparser.parse(url+lang+"/rss.xml")
+        if feed.status == 200:
+            for entry in feed.entries:
+                try:
+                    entry_guid = entry.guid.split(" ")[0]
+                except:
+                    continue
+                if entry_guid not in guids and not entry.title in titles:
+                    title = entry.title
+                    soup = BeautifulSoup(entry.description, "html.parser")
+                    description = soup.find('div').getText()
+                    image_url = nurl+soup.find('img')['src']
+                    
+                    post_to_discord(title, description, entry.link, image_url, entry.published_parsed, server_name)
 
-def post_to_discord(title, description, link, image, date):
-	payload = {
-		"username": "Quinn",
-		"avatar_url": "http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/Quinn.png",
-		"content": "",
-		"embeds": [
-		    {
-		        "title": title,
-		        "description": description,
-		        "url": link,
-		        "image": {"url": image},
-		        "color": 15258703,
-		        "timestamp": time.strftime('%Y-%m-%dT%H:%M:%SZ', date)
-		    }
-		]
-	}
-	print ("Posting to Discord...")
-	r = requests.post(WEBHOOK_URL, json=payload, headers={'Content-Type': 'multipart/form-data'})
-	print ("HTTP REQUEST: ", r.status_code, r.text)
+                    guids[entry_guid] = True
+                    titles[title] = True
 
-if __name__ == '__main__':	
-	print ("Fetching data from League of Legends' servers...")
-	fetch_data()
-	print ("Listening for new articles!")
-	while True:
-		check_for_new_articles()
-		time.sleep(10)
+def post_to_discord(title, description, link, image, date, server_name):
+    payload = {
+        "username": "Quinn",
+        "avatar_url": "http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/Quinn.png",
+        "content": "",
+        "embeds": [
+            {
+                "title": title,
+                "description": description,
+                "url": link,
+                "image": {"url": image},
+                "color": 15258703,
+                "timestamp": time.strftime('%Y-%m-%dT%H:%M:%SZ', date),
+                "footer": {"text": server_name}
+            }
+        ]
+    }
+    print ("Posting to Discord...")
+    for webhook in DATA['WEBHOOK_URL']:
+    	r = requests.post(webhook, json=payload, headers={'Content-Type': 'multipart/form-data'})
+    	print ("HTTP REQUEST: ", r.status_code, r.text)
+
+if __name__ == '__main__':  
+    print ("Fetching data from League of Legends' servers...")
+    fetch_data()
+    print ("Listening for new articles!")
+    while True:
+        try:
+            check_for_new_articles()
+            time.sleep(10)
+        except:
+            print ("An error has occurred, retrying in 10 secs.")
+            time.sleep(10)
